de3f13194b88e43c369b3fbe035c853c79ab51a3
João Victor <54048948+c4ng4c3ir0@users.noreply.github.com>
2021-03-05 19:51:50 -0300

    Update recon_js.sh

--- recon_js.sh
+++ recon_js.sh
@@ -1,14 +1,29 @@
 #/bin/bash
-echo "MADE BY C4NG4C3IR0"
+echo                    ░█▀▀░█░█░█▀█░█▀▀░█░█░█▀▄░█▀▀░█▀▀░█▀█░█▀█
+echo                    ░█░░░░▀█░█░█░█░█░░▀█░█▀▄░█▀▀░█░░░█░█░█░█
+echo                    ░▀▀▀░░░▀░▀░▀░▀▀▀░░░▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀
+echo
+echo
+echo
+
+
+echo "digite o nome do site (não a url)"
+read  pasta
+echo
+echo
+
+mkdir $pasta
+
+cd $pasta
 
 echo "Digite a url: "
 read url
-
+echo
+echo
 
 gau $url | grep "\.js" > salvarjs.txt
 
 cat salvarjs.txt  | httpx -mc 200 -silent  | tee saida.txt
 
-xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.py -i {} -o cli" | tee analisejs.txt
-
+xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.>
 cat analisejs.txt   | python3 /collector.py output
