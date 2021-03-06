201efb568b31cf378174d9d3cd7539af89f99295
João Victor <54048948+c4ng4c3ir0@users.noreply.github.com>
2021-03-06 11:14:01 -0300

    Update c4ng4recon.sh

--- c4ng4recon.sh
+++ c4ng4recon.sh
@@ -18,12 +18,12 @@ cd $pasta
 
 echo "Digite a url: "
 read url
-echo
-echo
+
 
 gau $url | grep "\.js" > salvarjs.txt
 
 cat salvarjs.txt  | httpx -mc 200 -silent  | tee saida.txt
 
-xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.>
+xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.py -i {} -o cli" | tee analisejs.txt
+
 cat analisejs.txt   | python3 /collector.py output
